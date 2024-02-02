72732446904a48c22f8a8e8ead937fac1ed0801b
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2024-02-02 13:46:05 +0100

    groupe + id des pays sur la carte du monde

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import MyCarteDuMonde from '@/components/MyCarteDuMonde.vue'
+</script>
 
 <template>
   <h1>Test Home Page</h1>
+  <MyCarteDuMonde />
 </template>
 
 <style lang="scss"></style>
