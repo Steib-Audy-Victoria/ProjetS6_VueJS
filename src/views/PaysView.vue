c855ca69d87eaf5f8690b00cc280997a4a8b3ec3
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2024-02-09 08:39:37 +0100

    changement grid chart

--- src/views/PaysView.vue
+++ src/views/PaysView.vue
@@ -317,7 +317,11 @@ export default {
 
 .GridChartJs {
   display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+
+  @include large-up {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
 }
 
 .MarqueLink {
